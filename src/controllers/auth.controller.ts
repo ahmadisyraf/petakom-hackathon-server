30472aa1d286ba9070e7d2b9e8d1e4562d0b48e9
ahmadisyraf <isyrafmagic@gmail.com>
2024-10-19 19:09:31 +0800

    update: add auth

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,38 @@
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
+import { Hono } from "hono";
+import bcrypt from "bcrypt";
+import { HTTPException } from "hono/http-exception";
+import { sign } from "hono/jwt";
+import prisma from "../lib/prisma";
+
+const auth = new Hono();
+
+auth.post(
+  "/",
+  zValidator(
+    "json",
+    z.object({
+      email: z
+        .string({ required_error: "Email required" })
+        .email({ message: "Invalid email" }),
+      password: z.string({ required_error: "Password required" }),
+    })
+  ),
+  async (c) => {
+    const { email, password } = c.req.valid("json");
+
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new HTTPException(404, { message: "Invalid user" });
+    }
+
+    const payload = { ...user, exp: Math.floor(Date.now() / 1000) + 60 * 5 }; // 5 minutes
+    const token = await sign(payload, "secret_key");
+
+    return c.json({ accessToken: token });
+  }
+);
+
+export default auth;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,17 @@
 import { Hono } from "hono";
 import users from "../controllers/users.controller";
+import auth from "../controllers/auth.controller";
+import { logger } from "hono/logger";
 
-const app = new Hono().basePath("/api");
+const app = new Hono();
+
+app.use(logger());
 
 app.route("/users", users);
+app.route("/auth", auth);
+
+app.notFound((c) => {
+  return c.text("Route not found", 404);
+});
 
 export default app;
