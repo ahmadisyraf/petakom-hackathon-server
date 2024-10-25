4bf5d2224a09f8ddc34d41d5f40b773b4e71954d
ahmadisyraf <isyrafmagic@gmail.com>
2024-10-25 17:36:08 +0800

    update: add donation

---
+++ src/controllers/donation.controller.ts
@@ -0,0 +1,135 @@
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { jwt } from "hono/jwt";
+import { z } from "zod";
+import prisma from "../lib/prisma";
+
+const donation = new Hono();
+
+donation.get(
+  "/",
+  jwt({ secret: "secret" }),
+  zValidator(
+    "query",
+    z.object({
+      status: z.enum(["completed", "pending", "reserved"]).optional(),
+    })
+  ),
+  async (c) => {
+    const { id } = c.get("jwtPayload");
+    const { status } = c.req.valid("query");
+
+    const getUserDonation = await prisma.donation.findMany({
+      where: {
+        AND: {
+          status,
+          user: { id },
+        },
+      },
+    });
+
+    return c.json(getUserDonation);
+  }
+);
+
+donation.post(
+  "/",
+  jwt({
+    secret: "secret",
+  }),
+  zValidator(
+    "json",
+    z.object({
+      title: z.string({ required_error: "Donation title required" }),
+      description: z.string({ required_error: "foodType" }),
+      donationSize: z.string({ required_error: "Donation size required" }),
+      foodType: z.string({ required_error: "Food type required" }),
+      transportationMethod: z.enum([
+        "request_for_pickup",
+        "deliver_to_organization",
+      ]),
+      status: z
+        .enum(["completed", "pending", "reserved"])
+        .optional()
+        .default("pending"),
+    })
+  ),
+  async (c) => {
+    const { id } = c.get("jwtPayload");
+    const data = c.req.valid("json");
+
+    const createdDonation = await prisma.donation.create({
+      data: {
+        ...data,
+        user: { connect: { id } },
+      },
+    });
+
+    return c.json(createdDonation);
+  }
+);
+
+donation.get(
+  "/:id",
+  jwt({ secret: "secret" }),
+  zValidator(
+    "param",
+    z.object({
+      id: z.string({ required_error: "Donation id required" }),
+    })
+  ),
+  async (c) => {
+    const { id } = c.req.valid("param");
+
+    const getDonation = await prisma.donation.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    return c.json(getDonation);
+  }
+);
+
+donation.patch(
+  "/:id",
+  jwt({ secret: "secret" }),
+  zValidator(
+    "param",
+    z.object({
+      id: z.string({ required_error: "Donation id required" }),
+    })
+  ),
+  zValidator(
+    "json",
+    z.object({
+      title: z.string({ required_error: "Donation title required" }).optional(),
+      description: z.string({ required_error: "foodType" }).optional(),
+      donationSize: z
+        .string({ required_error: "Donation size required" })
+        .optional(),
+      foodType: z.string({ required_error: "Food type required" }).optional(),
+      transportationMethod: z
+        .enum(["request_for_pickup", "deliver_to_organization"])
+        .optional(),
+      status: z.enum(["completed", "pending", "reserved"]).optional(),
+    })
+  ),
+  async (c) => {
+    const { id } = c.req.valid("param");
+    const data = c.req.valid("json");
+
+    const updatedData = await prisma.donation.update({
+      data: {
+        ...data,
+      },
+      where: {
+        id,
+      },
+    });
+
+    return c.json(updatedData);
+  }
+);
+
+export default donation;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,8 @@ import { Hono } from "hono";
 import users from "../controllers/users.controller";
 import auth from "../controllers/auth.controller";
 import { logger } from "hono/logger";
-import { authMiddleware } from "../middlewares/auth.middleware";
 import organization from "../controllers/organization.controller";
+import donation from "../controllers/donation.controller";
 
 const app = new Hono();
 
@@ -12,6 +12,7 @@ app.use(logger());
 app.route("/users", users);
 app.route("/auth", auth);
 app.route("/organization", organization);
+app.route("/donation", donation);
 
 app.notFound((c) => {
   return c.text("Route not found", 404);
