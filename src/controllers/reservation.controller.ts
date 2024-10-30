f040a8f7ad5444c41e7902eb0e3c6743a37223e7
ahmadisyraf <isyrafmagic@gmail.com>
2024-10-30 20:09:58 +0800

    update: get all current user reservation

--- src/controllers/reservation.controller.ts
+++ src/controllers/reservation.controller.ts
@@ -86,4 +86,16 @@ reservation.patch(
   }
 );
 
+reservation.get("/", jwt({ secret: "secret" }), async (c) => {
+  const { id } = c.get("jwtPayload");
+
+  const findReservation = await prisma.reservation.findMany({
+    where: {
+      organizationId: id,
+    },
+  });
+
+  return c.json(findReservation);
+});
+
 export default reservation;
